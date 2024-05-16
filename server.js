4e4cdd84de73f06618ceecfa15235683bf709070
hugo0515 <167876219+hugo0515@users.noreply.github.com>
2024-05-16 20:53:22 +0900

    ejs framework with frontend

---
+++ server.js
@@ -0,0 +1,33 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require('http');
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// 2: Session
+
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({test: "success"});
+});
+
+app.get("/", function (req, res){
+    res.render('harid');
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: `, PORT);
+})
